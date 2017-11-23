9869fddfddd8fba9548e5fb9618338eafc3971dd
projetocomfio <33916030+projetocomfio@users.noreply.github.com>
2017-11-23 11:23:59 -0300

    atuali

--- enviar.php
+++ enviar.php
@@ -1,7 +1,7 @@
 <?php  
-    $nome = $_POST['nome'];
+    $nome = $_POST['name'];
     $email = $_POST['email'];
-    $assunto = $_POST['assunto'];
+    $assunto = $_POST['subject'];
     $mensagem = $_POST['message'];
     $headers = "From: " . $nome;
     $corpoemail = 'E-mail enviado atraves do site
@@ -11,13 +11,14 @@
                     Assunto: '.$assunto.'
                     Mensagem: '.$mensagem;
     ini_set("smtp.gmail.com", 587);
-    $envio = mail("P2p.ufpe@gmail.com", "Contato", $corpoemail, $headers);
+    $envio = mail("p2p.ufpe@gmail.com", "Contato", $corpoemail, $headers);
     if ($envio) {
         echo "<script>alert('Mensagem enviada com sucesso, aguarde resposta');</script>";
         header('Location: index.html');
     }
     else{
         echo "<script>alert('Ocorreu um erro ao enviar mensagem, tente novamente mais tarde');</script>";
+        header('Location: index.html');
     }
 
 
